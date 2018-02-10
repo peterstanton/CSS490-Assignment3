a8d03e7663694ccfee32f730c8ebb61f36607e27
Peter Stanton <stantp2@uw.edu>
2018-02-09 22:06:56 -0800

    major progress

--- backup.py
+++ backup.py
@@ -1,42 +1,48 @@
 import boto3
 import os
-
+from boto3 import Session
 
 # https://stackoverflow.com/questions/26871884/how-can-i-easily-determine-if-a-boto-3-s3-bucket-resource-exists
-
 # https://stackoverflow.com/questions/7872611/in-python-what-is-the-difference-between-pass-and-return
+# Thanks to Roman Zhang for helping me understand Session objects in boto3.
+
 
 def recurse(s3, new_bucket, full_path, old_dir):
     for root, dirs, files in os.walk(full_path):
         curpath = root + '\\'
         curdir = old_dir + os.path.basename(os.path.normpath(curpath))
 
-        for dir in dirs:
+        for thisdir in dirs:
             parser = old_dir + '/' + os.path.basename(curdir)
-            recurse(s3, new_bucket, curpath + dir, parser.replace('//', '/') + '/')
+            recurse(s3, new_bucket, curpath + thisdir, parser.replace('//', '/') + '/')
 
         for file in files:
             s3.Object(new_bucket, curdir + '/' + file).put(Body=open(curpath + file, "rb"))
-            #                           should concatenate in path
-        # Loop through present direectories, should move up ABOVE the for loop, loop through dirs calling recurse.
         return
     return
 
 
-s3 = boto3.resource("s3")
+def getsession():
+    return Session(aws_access_key_id=input("Enter your AWS access key: "),
+                   aws_secret_access_key=input("Enter your AWS secret key: "),
+                   region_name=input("Enter the AWS region you wish to use: "))
 
+
+mysession = getsession()
+s3 = mysession.resource("s3")
 buckets = s3.buckets.all()
+print("These are your account's current S3 buckets: ")
+print("---------------------------------------------")
 for bucket in buckets:
     print(bucket.name)
 
-newBucket = input("new bucket name ")
+newBucket = input("Enter the name of a bucket to use for backing up: ")
 print("Creating bucket: ", newBucket)
 try:
-    s3.create_bucket(Bucket=newBucket, CreateBucketConfiguration={"LocationConstraint": "us-west-1"})
+    s3.create_bucket(Bucket=newBucket, CreateBucketConfiguration={"LocationConstraint": mysession.region_name})
 except:
-    print("This bucket already exists.")
+    print("This bucket already exists, continuing with existing bucket.")
 
 bucketName = newBucket
-fullPath = input("Input directory path ")
-
+fullPath = input("Input directory path for backup: ")
 recurse(s3, bucketName, fullPath, "Backup/")
