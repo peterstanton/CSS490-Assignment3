f91eb3b832b977114782d76d8274852c0ecd1d58
Peter Stanton <stantp2@uw.edu>
2018-02-09 21:11:36 -0800

    done?

--- backup.py
+++ backup.py
@@ -1,25 +1,26 @@
 import boto3
 import os
 
-# The bucket does not exist or you have no access.
 
 # https://stackoverflow.com/questions/26871884/how-can-i-easily-determine-if-a-boto-3-s3-bucket-resource-exists
 
-# I need a way to preserve folder structure by appending stuff to backup.
+# https://stackoverflow.com/questions/7872611/in-python-what-is-the-difference-between-pass-and-return
 
-def recurse(s3, new_bucket, full_path):
+def recurse(s3, new_bucket, full_path, old_dir):
     for root, dirs, files in os.walk(full_path):
         curpath = root + '\\'
-        curdir = os.path.basename(os.path.normpath(curpath))
+        curdir = old_dir + os.path.basename(os.path.normpath(curpath))
 
         for dir in dirs:
-            recurse(s3, new_bucket, curpath + dir)
+            parser = old_dir + '/' + os.path.basename(curdir)
+            recurse(s3, new_bucket, curpath + dir, parser.replace('//', '/') + '/')
 
         for file in files:
-            s3.Object(new_bucket, "Backup/" + curdir + '/' + file).put(Body=open(curpath + file, "rb"))
+            s3.Object(new_bucket, curdir + '/' + file).put(Body=open(curpath + file, "rb"))
             #                           should concatenate in path
         # Loop through present direectories, should move up ABOVE the for loop, loop through dirs calling recurse.
-    pass
+        return
+    return
 
 
 s3 = boto3.resource("s3")
@@ -30,9 +31,12 @@ for bucket in buckets:
 
 newBucket = input("new bucket name ")
 print("Creating bucket: ", newBucket)
-s3.create_bucket(Bucket=newBucket, CreateBucketConfiguration={"LocationConstraint": "us-west-1"})
+try:
+    s3.create_bucket(Bucket=newBucket, CreateBucketConfiguration={"LocationConstraint": "us-west-1"})
+except:
+    print("This bucket already exists.")
 
 bucketName = newBucket
 fullPath = input("Input directory path ")
 
-recurse(s3, bucketName, fullPath)
+recurse(s3, bucketName, fullPath, "Backup/")
