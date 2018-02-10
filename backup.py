dda305627860c7a65d1b59e0d2c824133058836a
Peter Stanton <stantp2@uw.edu>
2018-02-09 18:27:28 -0800

    update

--- backup.py
+++ backup.py
@@ -2,13 +2,13 @@ import boto3
 import os
 
 
-def recurse(s3, newBucket, fullPath):
-
-    for root, dirs, files in os.walk(fullPath):
-        # replace below with stuff I actually need to do.
+def recurse(s3, new_bucket, full_path):
+    for root, dirs, files in os.walk(full_path):
         for file in files:
             # upload file to bucket.
-            s3.Object(newBucket, "Backup/" + file).put(Body=open(fullPath + file, "rb"))
+            s3.Object(new_bucket, "Backup/" + file).put(Body=open(full_path + file, "rb"))
+            # s3.Object(existingbucket, "hello/" + filename).put(Body=open(fullpath + filename, "rb"))
+            #                           should concatenate in path
         # create next folder(s).
     pass
 
@@ -21,9 +21,9 @@ for bucket in buckets:
 
 newBucket = input("new bucket name")
 print("Creating bucket: ", newBucket)
-s3.create_bucket(Bucket=newBucket, CreateBucketConfiguration={"LocationConstraint": "ap-oregon-1"})
+s3.create_bucket(Bucket=newBucket, CreateBucketConfiguration={"LocationConstraint": "us-west-1"})
 
 bucketName = newBucket
 fullPath = input("Input directory path")
 
-recurse(s3, newBucket, fullPath)
+recurse(s3, bucketName, fullPath)
