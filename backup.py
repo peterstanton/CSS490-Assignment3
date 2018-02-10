f1f3ec157799168d972579d7335e402bd35641aa
Peter Stanton <stantp2@uw.edu>
2018-02-09 19:17:01 -0800

    progress

--- backup.py
+++ backup.py
@@ -10,11 +10,11 @@ from botocore.client import ClientError
 def recurse(s3, new_bucket, full_path):
     for root, dirs, files in os.walk(full_path):
         # String gets current local path
-
+        curDir = root + '\\'
 
         for file in files:
             # upload file to bucket.
-            s3.Object(new_bucket, "Backup/" + file).put(Body=open(full_path + file, "rb"))
+            s3.Object(new_bucket, "Backup/" + file).put(Body=open(curDir + file, "rb"))
             #                           should concatenate in path
         # Loop through present direectories, should move up ABOVE the for loop, loop through dirs calling recurse.
     pass
@@ -34,6 +34,6 @@ except ClientError:
     s3.create_bucket(Bucket=newBucket, CreateBucketConfiguration={"LocationConstraint": "us-west-1"})
 
 bucketName = newBucket
-fullPath = input("Input directory path, end with system-appropriate separator character ")
+fullPath = input("Input directory path ")
 
 recurse(s3, bucketName, fullPath)
